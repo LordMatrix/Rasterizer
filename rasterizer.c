0fc87c5df9adb8db3f5c5b1ba315a5617f6d12c1
LordMatrix <mrkvr84@gmail.com>
2017-05-11 17:26:41 +0200

    perfect rotating cube

--- rasterizer.c
+++ rasterizer.c
@@ -19,7 +19,7 @@
 // Definicion de un cubo en 3 formatos numericos diferentes
 // Entero, coma fija 8 bits y flotante
 
-#define LCF (100.0f)    // En flotantes
+#define LCF (200.0f)    // En flotantes
 #define PI 3.1416f
 
 
@@ -81,6 +81,7 @@ int getColorByIndex(int i) {
 }
 
 
+/// Draws a straight line given a pair of points
 void drawLine(unsigned int* pixels, int pitch, Point start, Point end) {
     //Bresenham's algorithm
     int dx = abs(end.x - start.x), sx = start.x < end.x ? 1 : -1;
@@ -101,34 +102,77 @@ void drawLine(unsigned int* pixels, int pitch, Point start, Point end) {
 
 
 
+/// Shortcuts for the rotation matrix for a given axis. Axis => 0=x, 1=y, 2=z
+/// Returns the rotated point
+static Point rotatePoint(Point p, float rads, int axis) {
+  float sinr = sin(rads);
+  float cosr = cos(rads);
+  Point trans;
+
+  switch(axis) {
+    // X-AXIS ROTATION
+    case 0:
+      trans.x = p.x;
+      trans.y = p.y*cosr - p.z*sinr;
+      trans.z = p.y*sinr + p.z*cosr;
+      break;
+    // Y-AXIS ROTATION
+    case 1:
+      trans.x = p.z*sinr + p.x*cosr;
+      trans.y = p.y;
+      trans.z = p.z*cosr - p.x*sinr;
+      break;
+    // Z-AXIS ROTATION (2D)
+    case 2:
+      trans.x = p.x*cosr - p.y*sinr;
+      trans.y = p.x*sinr + p.y*cosr;
+      trans.z = p.z;
+      break;
+    default:
+      trans = p;
+      break;
+  }
+
+
+  return trans;
+}
+
+
+
 static void PaintCubeInFloat ( unsigned int* pixels, float w, float h, int pitch, float trans_x, float trans_z, float proy, float rot) {   
   int i;
 
   Point points[8];
-  int indexes[24] = { 0,1, 1,2, 2,3, 0,3,
+  //Indices for drawing lines to connect edges
+  int indices[24] = { 0,1, 1,2, 2,3, 0,3,
                       4,5, 5,6, 6,7, 4,7,
                       0,4, 1,5, 2,6, 3,7};
 
-
+  //Transform and paint all vertices in cube
   for ( i=0; i<8; i++) { 
     float xp, yp;
     float x = cubef [ i * 3 + 0];
     float y = cubef [ i * 3 + 1];
     float z = cubef [ i * 3 + 2];
 
-//BROKEN ROTATION
-    float angle = i * ((2*PI) / 8.0f);
-    x += cos(rot+angle) * x;
-//    y = sin(rot+angle) * y;
+    //Construct point
+    Point p = makePoint(x, y, z);
 
-    z += trans_z;
+    //Rotate in all axis
+    p = rotatePoint(p, rot, 0);
+    p = rotatePoint(p, rot, 1);
+    p = rotatePoint(p, rot, 2);
 
-    xp = (x * proy) / z;
-    yp = (y * proy) / z;
+    //project Z axis on 2 dimensions
+    p.z -= trans_z;
+    xp = (p.x * proy) / p.z;
+    yp = (p.y * proy) / p.z;
+
+    //translate points to the middle of the screen
     xp += w * 0.5f;
     yp += h * 0.5f;
 
-
+    //Save point
     points[i] = makePoint(xp,yp,0);
 
     if (( xp >= 0) && (xp < w) && (yp >= 0) && (yp < h)) {
@@ -137,10 +181,10 @@ static void PaintCubeInFloat ( unsigned int* pixels, float w, float h, int pitch
     }
   }
 
-  //DRAW LINES
+  //DRAW LINE PRIMITIVES
   for (i=0; i<24; i+=2) {
-    Point start = points[indexes[i]];
-    Point end = points[indexes[i+1]];
+    Point start = points[indices[i]];
+    Point end = points[indices[i+1]];
     drawLine(pixels, pitch, start, end);
   }
   
@@ -176,8 +220,8 @@ int main ( int argc, char **argv) {
     // Girar los cubos en pantalla
     float x, z, offs_z = 1200.0f;
     float radius = 100.0f;
-    x = 0.0f   + radius * cos( ang + ((0.0f * PI * 2.0f) / 3.0f));
-    z = offs_z + radius * sin( ang + ((0.0f * PI * 2.0f) / 3.0f));
+    x = 0.0f   + radius;// * cos( ang + ((0.0f * PI * 2.0f) / 3.0f));
+    z = offs_z + radius;// * sin( ang + ((0.0f * PI * 2.0f) / 3.0f));
     PaintCubeInFloat ( g_screen_pixels, (float)g_SDLSrf->w, (float)g_SDLSrf->h, pitch, 
                        x, z, 
                        projection, ang);
